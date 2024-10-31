4808edb291850d971cbe37bed32d7f738424b3ed
marimonotamago <golupio24@gmail.com>
2024-10-31 17:20:07 +0900

    日報一覧画面実装　エラー

--- src/main/java/com/techacademy/repository/ReportsRepository.java
+++ src/main/java/com/techacademy/repository/ReportsRepository.java
@@ -1,9 +1,12 @@
 package com.techacademy.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import com.techacademy.entity.Employee;
 import com.techacademy.entity.Reports;
 
 public interface ReportsRepository extends JpaRepository<Reports, Integer> {
-
+    List<Reports> findByEmployee(Employee employee);;
 }
